784151ef6b82a1d0f1907d98516089d06628e493
Niclas <niclasjeppsson@gmail.com>
2022-06-20 19:26:39 +0100

    Initial Commit

---
+++ SignUpFormCombine/SignUpFormCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpFormCombineApp.swift
+//  SignUpFormCombine
+//
+//  Created by Niclas Jeppsson on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SignUpFormCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
